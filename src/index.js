11d769fd5985092ce927c3ae1c77411d8ab46a7e
Udison Abreu <udi0abreu@gmail.com>
2022-10-17 14:53:58 -0300

    chore: install react router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
 );
 
