de6fcd2627184bbfeec2301e4282cc31092b4287
Udison Abreu <udi0abreu@gmail.com>
2022-10-21 11:14:02 -0300

    feat: created header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+      Home
+    </div>
   );
 }
