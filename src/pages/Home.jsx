858dedde9f3c1f0c6e8cdf86ec9a61d7405624f1
Udison Abreu <udi0abreu@gmail.com>
2022-10-30 17:00:06 -0300

    feat: home page component and css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
+import minimalistAvatar from '../images/minimalist-avatar.png';
+import '../css/home.css';
 
 export default function Home() {
   return (
-    <div>
+    <section className="home-container">
       <Header />
-      Home
-    </div>
+      <img className="minimalist-avatar" src={minimalistAvatar} alt="my minimalist avatar" />
+
+      <p className="introduction">
+        {
+          `Oi, meu nome é Udison
+          e eu sou um desenvolvedor front-end apaixonado
+          por interfaces bonitas e intuitivas.`
+        }
+      </p>
+    </section>
   );
 }
